9654b855d5d458d663738b5a7387c41bbca7fad7
khi <llioopv@naver.com>
2024-09-30 22:43:36 +0900

    Initial Commit

---
+++ EconomyStock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EconomyStock
+//
+//  Created by 권형일 on 9/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EconomyStock/EconomyStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  EconomyStockApp.swift
+//  EconomyStock
+//
+//  Created by 권형일 on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct EconomyStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
