d2cec63ae3ec8737454ec09aa70729e45bba6d3b
khi <llioopv@naver.com>
2024-10-01 13:13:20 +0900

    Add basic firebase environment code

---
+++ EconomyStock/Component/Splash/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  EconomyStock
+//
+//  Created by 권형일 on 10/1/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text("Splash View")
+    }
+}
+
+#Preview {
+    SplashView()
+}
