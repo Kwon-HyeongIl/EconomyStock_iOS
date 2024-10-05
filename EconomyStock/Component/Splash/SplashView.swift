cf2fd4113ccd3df8257931c52f3027d74d271d59
khi <llioopv@naver.com>
2024-10-05 18:57:07 +0900

    Update SplashView UI design

--- EconomyStock/Component/Splash/SplashView.swift
+++ EconomyStock/Component/Splash/SplashView.swift
@@ -8,28 +8,12 @@
 import SwiftUI
 
 struct SplashView: View {
-    @State private var isExpand = false
     @State private var isCharacterMovedUp = false
     @State private var isTitleLogoVisible = false
 
     var body: some View {
         ZStack {
-            Circle()
-                .fill(Color.ESTitle)
-                .frame(width: isExpand ? UIScreen.main.bounds.height * 2 : 100, height: isExpand ? UIScreen.main.bounds.height * 2 : 100)
-                .scaleEffect(isExpand ? 1 : 0)
-                .animation(.easeIn(duration: 1.5), value: isExpand)
-                .onAppear {
-                    isExpand = true
-                    
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                        isCharacterMovedUp = true
-                        
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-                            isTitleLogoVisible = true
-                        }
-                    }
-                }
+            Color.ESTitle
             
             VStack {
                 ZStack {
@@ -52,6 +36,15 @@ struct SplashView: View {
                         .padding(.top, 40)
                 }
                 .padding(.bottom, 30)
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    isCharacterMovedUp = true
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+                        isTitleLogoVisible = true
+                    }
+                }
             }
         }
         .ignoresSafeArea()
