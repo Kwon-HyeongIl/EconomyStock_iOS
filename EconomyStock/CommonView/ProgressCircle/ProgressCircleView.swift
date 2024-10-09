a7900479fb1b368d615b7525885ba4a4d691df10
khi <llioopv@naver.com>
2024-10-10 01:04:52 +0900

    Update animation in ProgressCircleView

--- EconomyStock/CommonView/ProgressCircle/ProgressCircleView.swift
+++ EconomyStock/CommonView/ProgressCircle/ProgressCircleView.swift
@@ -9,52 +9,52 @@ import SwiftUI
 
 struct ProgressCircleView: View {
     @State private var animatedProgressRate: Double = 0
-    @State private var timer: Timer?
-    
+
     let progressRate: Double
-    
+
     var body: some View {
         ZStack {
+            // 배경 원 테두리
             Circle()
                 .stroke(Color.gray.opacity(0.2), lineWidth: 4)
                 .frame(width: 30, height: 30)
-            
+
+            // 진행률 원 테두리
             Circle()
                 .trim(from: 0, to: animatedProgressRate / 100)
                 .stroke(Color.gray, lineWidth: 3)
                 .frame(width: 30, height: 30)
                 .rotationEffect(Angle(degrees: -90))
-            
-            Text("\(Int(animatedProgressRate))%")
-                .font(.system(size: 8))
-                .foregroundColor(.gray)
-                .fontWeight(.semibold)
+
+            // 진행률 텍스트
+            AnimatableNumberText(value: animatedProgressRate)
         }
         .onAppear {
-            let duration = 1.5
-            let updateInterval = 0.02
-            let totalSteps = Int(duration / updateInterval)
-            var currentStep = 0
-            let increment = progressRate / Double(totalSteps)
-            
-            timer = Timer.scheduledTimer(withTimeInterval: updateInterval, repeats: true) { _ in
-                if currentStep >= totalSteps {
-                    timer?.invalidate()
-                    timer = nil
-                    animatedProgressRate = progressRate
-                } else {
-                    animatedProgressRate += increment
-                    currentStep += 1
-                }
+            withAnimation(.easeOut(duration: 1.5)) {
+                animatedProgressRate = progressRate
             }
-        }
-        .onDisappear {
-            timer?.invalidate()
-            timer = nil
         }
     }
 }
 
+// 숫자가 1 ~ target number 까지 차례대로 변하기 위해서 Animatable 구조체 사용
+struct AnimatableNumberText: Animatable, View {
+    var value: Double
+
+    // Animatable 프로토콜 필수 구현 연산 프로퍼티 (네이밍 고정)
+    var animatableData: Double {
+        get { value }
+        set { value = newValue }
+    }
+
+    var body: some View {
+        Text("\(Int(value))%")
+            .font(.system(size: 8))
+            .foregroundColor(.gray)
+            .fontWeight(.semibold)
+    }
+}
+
 #Preview {
-    ProgressCircleView(progressRate: 0.5)
+    ProgressCircleView(progressRate: 90)
 }
