80085475916faa8992d7b9c19d40e815cff6c654
khi <llioopv@naver.com>
2024-11-02 22:48:52 +0900

    Update BasicEconomy1 page

--- EconomyStock/VVM/MainTab/Studying/View/Course/1_BasicEconomy/BasicEconomy1View.swift
+++ EconomyStock/VVM/MainTab/Studying/View/Course/1_BasicEconomy/BasicEconomy1View.swift
@@ -13,6 +13,10 @@ struct BasicEconomy1View: View {
     
     @State private var progress: [Int] = []
     
+    @State private var building = false
+    @State private var governmentBuilding = false
+    @State private var earth = false
+    
     var body: some View {
         VStack {
             HStack {
@@ -25,29 +29,84 @@ struct BasicEconomy1View: View {
             }
             
             if progress.count >= 1 {
-                VStack {
-                    Text("우리나라에서 경제의 주체는 \n가계, 기업, 정부, 해외부문으로 나뉘어요")
-                        .font(.system(size: 20))
-                        .fontWeight(.semibold)
-                }
+                (Text("우리나라에서 경제의 주체는 \n")
+                 + Text("가계, 기업, 정부, 해외부문")
+                    .foregroundStyle(Color.ESTitle)
+                 + Text("으로 나뉘어요")
+                )
+                .font(.system(size: 20))
+                .fontWeight(.semibold)
                 .padding(.top, 20)
+            }
+            
+            if progress.count >= 2 {
+                VStack(spacing: 20) {
+                    HStack(spacing: 50) {
+                        LottieViewConverter(fileName: "BasicEconomy1_Home", loopMode: .playOnce, width: 100, height: 100)
+                        
+                        if building {
+                            LottieViewConverter(fileName: "BasicEconomy1_Building",  loopMode: .playOnce, toProgress: 0.8, width: 100, height: 100)
+                        }
+                            
+                    }
+                    
+                    Text("경제")
+                        .font(.system(size: 25))
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color.ESTitle)
+                        .padding(.top, 20)
+                    
+                    HStack(spacing: 50) {
+                        if governmentBuilding {
+                            LottieViewConverter(fileName: "BasicEconomy1_GovernmentBuilding", loopMode: .playOnce, width: 100, height: 100)
+                        }
+                        
+                        if earth {
+                            LottieViewConverter(fileName: "BasicEconomy1_Earth", loopMode: .playOnce, toProgress: 0.7, width: 110, height: 110)
+                            
+                        }
+                    }
+                }
+                .padding(.top, 50)
             }
             
             Spacer()
             
-            Button {
-                viewModel.currentPage += 1
-                navigationRouter.navigate(.BasicEconomy2View(viewModel))
-            } label: {
-                Image(systemName: "chevron.right")
-                    .modifier(CourseMoveButtonModifier())
+            if progress.count >= 2 {
+                Button {
+                    viewModel.currentPage += 1
+                    navigationRouter.navigate(.BasicEconomy2View(viewModel))
+                } label: {
+                    Image(systemName: "chevron.right")
+                        .modifier(CourseMoveButtonModifier())
+                }
             }
         }
         .modifier(CourseToolbarModifier(viewModel: viewModel, currentPage: viewModel.currentPage, totalPage: viewModel.course.totalPage))
+        .contentShape(Rectangle())
         .onTapGesture {
-            print("클릭")
-            withAnimation(.smooth(duration: 0.4)) {
+            withAnimation(.smooth(duration: 1.0)) {
                 progress.append(1)
+                
+                if progress.count == 2 {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        withAnimation(.smooth(duration: 1.0)) {
+                            building = true
+                        }
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        withAnimation(.smooth(duration: 1.0)) {
+                            governmentBuilding = true
+                        }
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation(.smooth(duration: 1.0)) {
+                            earth = true
+                        }
+                    }
+                }
             }
         }
     }
