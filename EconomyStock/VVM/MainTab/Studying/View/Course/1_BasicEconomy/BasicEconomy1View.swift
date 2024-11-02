495cd012b24ccfc263c4fe4caeaae4cee83f1eaf
khi <llioopv@naver.com>
2024-11-02 20:28:47 +0900

    Add tab progress logic

--- EconomyStock/VVM/MainTab/Studying/View/Course/1_BasicEconomy/BasicEconomy1View.swift
+++ EconomyStock/VVM/MainTab/Studying/View/Course/1_BasicEconomy/BasicEconomy1View.swift
@@ -11,8 +11,28 @@ struct BasicEconomy1View: View {
     @Environment(NavigationRouter.self) var navigationRouter
     @Bindable var viewModel: CourseViewModel
     
+    @State private var progress: [Int] = []
+    
     var body: some View {
         VStack {
+            HStack {
+                Text("경제주체")
+                    .font(.system(size: 25))
+                    .fontWeight(.bold)
+                    .padding(.leading)
+                
+                Spacer()
+            }
+            
+            if progress.count >= 1 {
+                VStack {
+                    Text("우리나라에서 경제의 주체는 \n가계, 기업, 정부, 해외부문으로 나뉘어요")
+                        .font(.system(size: 20))
+                        .fontWeight(.semibold)
+                }
+                .padding(.top, 20)
+            }
+            
             Spacer()
             
             Button {
@@ -24,6 +44,12 @@ struct BasicEconomy1View: View {
             }
         }
         .modifier(CourseToolbarModifier(viewModel: viewModel, currentPage: viewModel.currentPage, totalPage: viewModel.course.totalPage))
+        .onTapGesture {
+            print("클릭")
+            withAnimation(.smooth(duration: 0.4)) {
+                progress.append(1)
+            }
+        }
     }
 }
 
