57584d1a77c7927c61bcc07d1571d325ae27ec07
khi <llioopv@naver.com>
2024-11-12 20:29:45 +0900

    Update PriceLevel views basic setting

--- EconomyStock/VVM/MainTab/Studying/View/Course/2_PriceLevel/PriceLevel1View.swift
+++ EconomyStock/VVM/MainTab/Studying/View/Course/2_PriceLevel/PriceLevel1View.swift
@@ -11,11 +11,86 @@ struct PriceLevel1View: View {
     @Environment(NavigationRouter.self) var navigationRouter
     @Bindable var viewModel: CourseViewModel
     
+    @State private var progress: [Int] = []
+    
+    @State private var contentPart1 = false
+    @State private var contentPart2 = false
+    
+    @State private var nextButton = false
+    
     var body: some View {
-        Text("Price")
+        VStack {
+            ZStack {
+                ScrollView {
+                    HStack {
+                        Text("1.")
+                            .font(.system(size: 35))
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.ESTitle)
+                            .padding(.leading, 30)
+                            .padding(.bottom, 3)
+                        
+                        Text("물가의 개념")
+                            .font(.system(size: 25))
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                    }
+                    .opacity(0.2)
+                    .padding(.top)
+                    
+                    HStack {
+                        Text("정부지출을 위한 재원은 어떻게 조달될까요?")
+                            .font(.system(size: 20))
+                            .fontWeight(.semibold)
+                            .padding(.leading, 30)
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 5)
+                    
+                }
+                
+                if nextButton {
+                    VStack {
+                        Spacer()
+                        
+                        Button {
+                            let view = UIView(frame: .zero)
+                            UIImpactFeedbackGenerator(style: .light, view: view).impactOccurred()
+                            
+                            viewModel.currentPage += 1
+                            navigationRouter.navigate(.BasicEconomy2View(viewModel))
+                        } label: {
+                            LottieViewConverter(fileName: "CourseNextButton", loopMode: .playOnce, speed: 0.5, scale: 2.0, width: 100, height: 100)
+                                .shadow(color: .gray.opacity(0.5), radius: 10, x: 5, y: 5)
+                        }
+                    }
+                }
+            }
+        }
+        .modifier(CourseToolbarModifier(viewModel: viewModel, currentPage: viewModel.currentPage, totalPage: viewModel.course.totalPage))
+        .contentShape(Rectangle())
+        .onTapGesture {
+            withAnimation(.smooth(duration: 1.0)) {
+                if progress.count < 2 {
+                    progress.append(1)
+                }
+                
+                if progress.count == 1 {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
+                        withAnimation(.smooth(duration: 1.0)) {
+                            nextButton = true
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
     PriceLevel1View(viewModel: CourseViewModel(course: .DUMMY_COURSE))
+        .environment(NavigationRouter())
+        .environment(CourseListViewCapsule())
 }
--- EconomyStock/VVM/MainTab/Studying/View/Course/2_PriceLevel/PriceLevel5View.swift
+++ EconomyStock/VVM/MainTab/Studying/View/Course/2_PriceLevel/PriceLevel5View.swift
@@ -11,11 +11,115 @@ struct PriceLevel5View: View {
     @Environment(NavigationRouter.self) var navigationRouter
     @Bindable var viewModel: CourseViewModel
     
+    @State private var progress: [Int] = []
+    
+    @State private var contentPart1 = false
+    @State private var contentPart2 = false
+    
+    @State private var nextButton = false
+    @State private var beforeButton = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                ScrollView {
+                    HStack {
+                        Text("1.")
+                            .font(.system(size: 35))
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.ESTitle)
+                            .padding(.leading, 30)
+                            .padding(.bottom, 3)
+                        
+                        Text("물가의 개념")
+                            .font(.system(size: 25))
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                    }
+                    .opacity(0.2)
+                    .padding(.top)
+                    
+                    HStack {
+                        Text("정부지출을 위한 재원은 어떻게 조달될까요?")
+                            .font(.system(size: 20))
+                            .fontWeight(.semibold)
+                            .padding(.leading, 30)
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 5)
+                    
+                }
+                
+                if nextButton {
+                    VStack {
+                        Spacer()
+                        
+                        ZStack {
+                            Button {
+                                let view = UIView(frame: .zero)
+                                UIImpactFeedbackGenerator(style: .light, view: view).impactOccurred()
+                                
+                                viewModel.currentPage += 1
+                                navigationRouter.navigate(.BasicEconomy10View(viewModel))
+                            } label: {
+                                LottieViewConverter(fileName: "CourseNextButton", loopMode: .playOnce, speed: 0.5, scale: 2.0, width: 100, height: 100)
+                                    .shadow(color: .gray.opacity(0.5), radius: 10, x: 5, y: 5)
+                            }
+                            
+                            if beforeButton {
+                                HStack {
+                                    Button {
+                                        viewModel.currentPage -= 1
+                                        navigationRouter.back()
+                                    } label: {
+                                        Image(systemName: "chevron.left")
+                                            .font(.system(size: 25))
+                                            .fontWeight(.semibold)
+                                            .foregroundStyle(Color.ESTitle)
+                                            .padding()
+                                            .shadow(color: .gray.opacity(0.5), radius: 10, x: 5, y: 5)
+                                    }
+                                    
+                                    Spacer()
+                                }
+                                .padding(.leading, 60)
+                                .padding(.trailing, 70)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .modifier(CourseToolbarModifier(viewModel: viewModel, currentPage: viewModel.currentPage, totalPage: viewModel.course.totalPage))
+        .contentShape(Rectangle())
+        .onTapGesture {
+            withAnimation(.smooth(duration: 1.0)) {
+                if progress.count < 2 {
+                    progress.append(1)
+                }
+                
+                if progress.count == 1 {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
+                        withAnimation(.smooth(duration: 1.0)) {
+                            nextButton = true
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                withAnimation(.smooth(duration: 1.0)) {
+                                    beforeButton = true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
     PriceLevel5View(viewModel: CourseViewModel(course: Course.DUMMY_COURSE))
+        .environment(NavigationRouter())
+        .environment(CourseListViewCapsule())
 }
