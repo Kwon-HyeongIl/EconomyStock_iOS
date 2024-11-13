b7fe3032f9b4806285dec418c947f7fa23a3de28
Kwon-HyeongIl <llioopv@naver.com>
2024-11-13 22:35:20 +0900

    Update PriceLevelView 1, 2

--- EconomyStock/VVM/MainTab/Studying/View/Course/2_PriceLevel/PriceLevel1View.swift
+++ EconomyStock/VVM/MainTab/Studying/View/Course/2_PriceLevel/PriceLevel1View.swift
@@ -13,8 +13,7 @@ struct PriceLevel1View: View {
     
     @State private var progress: [Int] = []
     
-    @State private var contentPart1 = false
-    @State private var contentPart2 = false
+    @State private var image = false
     
     @State private var nextButton = false
     
@@ -36,19 +35,30 @@ struct PriceLevel1View: View {
                         
                         Spacer()
                     }
-                    .opacity(0.2)
                     .padding(.top)
                     
-                    HStack {
-                        Text("정부지출을 위한 재원은 어떻게 조달될까요?")
+                    if progress.count >= 1 {
+                        Text("물가란, 한 경제 내의 재화와 서비스의 전반적인 가격수준을 의미해요")
                             .font(.system(size: 20))
                             .fontWeight(.semibold)
-                            .padding(.leading, 30)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                            .padding(.top)
                         
-                        Spacer()
+                        if image {
+                            LottieViewConverter(fileName: "PriceLevel1_PriceProduct", loopMode: .playOnce, width: 150, height: 150)
+                                .padding(.top)
+                        }
                     }
-                    .padding(.top, 5)
                     
+                    if progress.count >= 2 {
+                        Text("물가는 시장에서 거래되는 개별상품의 가격을 경제에서 차지하는 중요도에 따라 가중치를 적용한 평균값을 통해 도출해요")
+                            .font(.system(size: 20))
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                            .padding(.top, 120)
+                    }
                 }
                 
                 if nextButton {
@@ -60,7 +70,7 @@ struct PriceLevel1View: View {
                             UIImpactFeedbackGenerator(style: .light, view: view).impactOccurred()
                             
                             viewModel.currentPage += 1
-                            navigationRouter.navigate(.BasicEconomy2View(viewModel))
+                            navigationRouter.navigate(.PriceLevel2View(viewModel))
                         } label: {
                             LottieViewConverter(fileName: "CourseNextButton", loopMode: .playOnce, speed: 0.5, scale: 2.0, width: 100, height: 100)
                                 .shadow(color: .gray.opacity(0.5), radius: 10, x: 5, y: 5)
@@ -73,12 +83,20 @@ struct PriceLevel1View: View {
         .contentShape(Rectangle())
         .onTapGesture {
             withAnimation(.smooth(duration: 1.0)) {
-                if progress.count < 2 {
+                if progress.count < 3 {
                     progress.append(1)
                 }
                 
                 if progress.count == 1 {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        withAnimation(.smooth(duration: 1.0)) {
+                            image = true
+                        }
+                    }
+                }
+                
+                if progress.count == 2 {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
                         withAnimation(.smooth(duration: 1.0)) {
                             nextButton = true
                         }
--- EconomyStock/VVM/MainTab/Studying/View/Course/2_PriceLevel/PriceLevel2View.swift
+++ EconomyStock/VVM/MainTab/Studying/View/Course/2_PriceLevel/PriceLevel2View.swift
@@ -13,8 +13,8 @@ struct PriceLevel2View: View {
     
     @State private var progress: [Int] = []
     
-    @State private var contentPart1 = false
-    @State private var contentPart2 = false
+    @State private var image1 = false
+    @State private var text2_2 = false
     
     @State private var nextButton = false
     @State private var beforeButton = false
@@ -40,16 +40,32 @@ struct PriceLevel2View: View {
                     .opacity(0.2)
                     .padding(.top)
                     
-                    HStack {
-                        Text("정부지출을 위한 재원은 어떻게 조달될까요?")
+                    if progress.count >= 1 {
+                        Text("물가의 중요한 특징 중 하나는, 물가와 화폐 가치가 반비례한다는 사실이에요")
                             .font(.system(size: 20))
                             .fontWeight(.semibold)
-                            .padding(.leading, 30)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                            .padding(.top)
                         
-                        Spacer()
+                        if image1 {
+                            LottieViewConverter(fileName: "PriceLevel2_PriceBalance", loopMode: .playOnce, speed:0.7, scale: 1.2, width: 150, height: 150)
+                                .padding(.top)
+                        }
                     }
-                    .padding(.top, 5)
                     
+                    if progress.count >= 2 {
+                        Text("예를 들어, 사과를 좋아하는 톡톡이는 작년까지 1000원을 주고 사과를 사먹었는데, 올해에 물가가 상승하여 사과가 1200원이 되었다고 가정해볼게요")
+                            .font(.system(size: 20))
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                            .padding(.top)
+                        
+                        if text2_2 {
+                            
+                        }
+                    }
                 }
                 
                 if nextButton {
@@ -96,11 +112,19 @@ struct PriceLevel2View: View {
         .contentShape(Rectangle())
         .onTapGesture {
             withAnimation(.smooth(duration: 1.0)) {
-                if progress.count < 2 {
+                if progress.count < 3 {
                     progress.append(1)
                 }
                 
                 if progress.count == 1 {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        withAnimation(.smooth(duration: 1.0)) {
+                            image1 = true
+                        }
+                    }
+                }
+                
+                if progress.count == 2 {
                     DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
                         withAnimation(.smooth(duration: 1.0)) {
                             nextButton = true
