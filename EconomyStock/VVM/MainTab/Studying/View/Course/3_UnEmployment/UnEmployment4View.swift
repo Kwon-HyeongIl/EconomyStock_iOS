fd19eb7332cea1097b5bccfa78c11e8b4ac6aeb4
Kwon-HyeongIl <llioopv@naver.com>
2024-11-17 19:25:07 +0900

    Finish UnEmployment4View

--- EconomyStock/VVM/MainTab/Studying/View/Course/3_UnEmployment/UnEmployment4View.swift
+++ EconomyStock/VVM/MainTab/Studying/View/Course/3_UnEmployment/UnEmployment4View.swift
@@ -13,121 +13,186 @@ struct UnEmployment4View: View {
     
     @State private var progress: [Int] = []
     
+    @State private var contentText1_2 = false
+    @State private var contentImage = false
+    @State private var imageAndBubble = false
+    @State private var bubbleContent = false
+    
     @State private var nextButton = false
     @State private var beforeButton = false
     
     var body: some View {
-        ScrollViewReader { proxy in
-            VStack {
-                ZStack {
-                    ScrollView {
-                        HStack {
-                            Text("1.")
-                                .font(.system(size: 35))
-                                .fontWeight(.bold)
-                                .foregroundStyle(Color.ESTitle)
-                                .padding(.leading, 30)
-                                .padding(.bottom, 3)
-                            
-                            Text("물가의 개념")
-                                .font(.system(size: 25))
-                                .fontWeight(.bold)
-                            
-                            Spacer()
-                        }
-                        .opacity(0.2)
-                        .padding(.top)
+        VStack {
+            ZStack {
+                ScrollView {
+                    HStack {
+                        Text("2.")
+                            .font(.system(size: 35))
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.ESTitle)
+                            .padding(.leading, 30)
+                            .padding(.bottom, 3)
                         
-                        ZStack {
-                            VStack {
-                                if progress.count >= 1 {
-                                    (Text("물가의 중요한 특징 중 하나는, 물가와 화폐 가치가 ")
-                                     + Text("반비례")
-                                        .foregroundStyle(Color.ESTitle)
-                                        .fontWeight(.bold)
-                                     + Text("한다는 사실이에요"))
+                        Text("고용 지표")
+                            .font(.system(size: 25))
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                    }
+                    .opacity(0.2)
+                    .padding(.top)
+                    
+                    VStack {
+                        if progress.count >= 1 {
+                            (Text("고용은 ")
+                             + Text("소비를 좌우")
+                                .foregroundStyle(Color.ESTitle)
+                                .fontWeight(.bold)
+                             + Text("하기 때문에 실업률과 고용률 등의 고용지표가 정말 중요해요"))
+                            .font(.system(size: 20))
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                            .padding(.top)
+                            
+                            if contentText1_2 {
+                                Text("경제 내의 실업이 감소하게 되어 일을 하는 사람들이 많아진다면 사람들의 소비가 증가하게 되겠죠?")
                                     .font(.system(size: 20))
                                     .fontWeight(.semibold)
                                     .multilineTextAlignment(.center)
                                     .padding(.horizontal)
-                                    .padding(.top)
-                                    
-                                }
-                                
-                                Spacer()
+                                    .padding(.top, 20)
                             }
                             
-                            Rectangle()
-                                .fill(.clear)
-                                .frame(width: 100, height: 200)
-                        }
-                        
-                        Rectangle()
-                            .fill(.clear)
-                            .frame(width: 100, height: 85)
-                            .id("bottom")
-                    }
-                    
-                    if nextButton {
-                        VStack {
-                            Spacer()
-                            
-                            ZStack {
-                                Button {
-                                    let view = UIView(frame: .zero)
-                                    UIImpactFeedbackGenerator(style: .light, view: view).impactOccurred()
-                                    
-                                    viewModel.currentPage += 1
-                                    navigationRouter.navigate(.PriceLevel3View(viewModel))
-                                } label: {
-                                    LottieViewConverter(fileName: "CourseNextButton", loopMode: .playOnce, speed: 0.5, scale: 2.0, width: 100, height: 100)
-                                        .shadow(color: .gray.opacity(0.5), radius: 10, x: 5, y: 5)
-                                }
-                                
-                                if beforeButton {
+                            if contentImage {
+                                ZStack {
                                     HStack {
-                                        Button {
-                                            viewModel.currentPage -= 1
-                                            navigationRouter.back()
-                                        } label: {
-                                            Image(systemName: "chevron.left")
-                                                .font(.system(size: 25))
-                                                .fontWeight(.semibold)
-                                                .foregroundStyle(Color.ESTitle)
-                                                .padding()
-                                                .shadow(color: .gray.opacity(0.5), radius: 10, x: 5, y: 5)
-                                        }
+                                        LottieViewConverter(fileName: "UnEmployment4_Factory", loopMode: .playOnce, width: 140, height: 140)
+                                            .padding(.bottom, 150)
                                         
                                         Spacer()
+                                        
+                                        LottieViewConverter(fileName: "UnEmployment4_Store", loopMode: .playOnce, width: 140, height: 140)
+                                            .padding(.top, 170)
                                     }
-                                    .padding(.leading, 60)
-                                    .padding(.trailing, 70)
+                                    
+                                    if imageAndBubble {
+                                        ZStack {
+                                            LottieViewConverter(fileName: "UnEmployment4_Road", loopMode: .playOnce, fromProgress: 0.0, toProgress: 0.0, width: 130, height: 130)
+                                                .rotationEffect(.degrees(-10.0))
+                                                .padding(.trailing, 60)
+                                                .padding(.top, 95)
+                                            
+                                            HStack {
+                                                Image("Run1_Toktok")
+                                                    .resizable()
+                                                    .scaledToFit()
+                                                    .frame(width: 60)
+                                                    .padding(.top, 80)
+                                                    .padding(.leading, 50)
+                                                
+                                                ZStack {
+                                                    LottieViewConverter(fileName: "Bubble", loopMode: .playOnce, scale: 3.2, width: 100, height: 100)
+                                                        .padding(.bottom, 70)
+                                                    
+                                                    if bubbleContent {
+                                                        Text("퇴근 했으니까\n쇼핑하러 가야지~")
+                                                            .font(.system(size: 13))
+                                                            .fontWeight(.semibold)
+                                                            .padding(.bottom, 78)
+                                                            .padding(.leading, 10)
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                
+                if nextButton {
+                    VStack {
+                        Spacer()
+                        
+                        ZStack {
+                            Button {
+                                let view = UIView(frame: .zero)
+                                UIImpactFeedbackGenerator(style: .light, view: view).impactOccurred()
+                                
+                                viewModel.currentPage += 1
+                                navigationRouter.navigate(.UnEmployment5View(viewModel))
+                            } label: {
+                                LottieViewConverter(fileName: "CourseNextButton", loopMode: .playOnce, speed: 0.5, scale: 2.0, width: 100, height: 100)
+                                    .shadow(color: .gray.opacity(0.5), radius: 10, x: 5, y: 5)
+                            }
+                            
+                            if beforeButton {
+                                HStack {
+                                    Button {
+                                        viewModel.currentPage -= 1
+                                        navigationRouter.back()
+                                    } label: {
+                                        Image(systemName: "chevron.left")
+                                            .font(.system(size: 25))
+                                            .fontWeight(.semibold)
+                                            .foregroundStyle(Color.ESTitle)
+                                            .padding()
+                                            .shadow(color: .gray.opacity(0.5), radius: 10, x: 5, y: 5)
+                                    }
+                                    
+                                    Spacer()
+                                }
+                                .padding(.leading, 60)
+                                .padding(.trailing, 70)
                             }
                         }
                     }
                 }
             }
-            .modifier(CourseToolbarModifier(viewModel: viewModel, currentPage: viewModel.currentPage, totalPage: viewModel.course.totalPage))
-            .contentShape(Rectangle())
-            .onTapGesture {
-                withAnimation(.smooth(duration: 1.0)) {
-                    if progress.count < 3 {
-                        progress.append(1)
-                    }
-                    
-                    if progress.count == 2 {
-                        withAnimation {
-                            proxy.scrollTo("bottom", anchor: .bottom)
-                        }
-                        
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
-                            withAnimation(.smooth(duration: 1.0)) {
-                                nextButton = true
-                                
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                    withAnimation(.smooth(duration: 1.0)) {
-                                        beforeButton = true
+        }
+        .modifier(CourseToolbarModifier(viewModel: viewModel, currentPage: viewModel.currentPage, totalPage: viewModel.course.totalPage))
+        .contentShape(Rectangle())
+        .onTapGesture {
+            withAnimation(.smooth(duration: 1.0)) {
+                if progress.count < 3 {
+                    progress.append(1)
+                }
+                
+                if progress.count == 1 {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        withAnimation(.smooth(duration: 1.0)) {
+                            contentText1_2 = true
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                withAnimation(.smooth(duration: 1.0)) {
+                                    contentImage = true
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                        withAnimation(.smooth(duration: 1.0)) {
+                                            imageAndBubble = true
+                                            
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                                withAnimation(.smooth(duration: 1.0)) {
+                                                    bubbleContent = true
+                                                    
+                                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                                        withAnimation(.smooth(duration: 1.0)) {
+                                                            nextButton = true
+                                                            
+                                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                                                withAnimation(.smooth(duration: 1.0)) {
+                                                                    beforeButton = true
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
