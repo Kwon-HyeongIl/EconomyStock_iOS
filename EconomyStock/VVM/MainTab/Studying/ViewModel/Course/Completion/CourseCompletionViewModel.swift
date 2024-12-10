043a492ee60077caf62d3a4443d1cd9703b9d6e8
Kwon-HyeongIl <llioopv@naver.com>
2024-12-10 17:19:52 +0900

    Update folder hierarchy

---
+++ EconomyStock/VVM/MainTab/Studying/ViewModel/Course/Completion/CourseCompletionViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  CourseCompletionViewModel.swift
+//  EconomyStock
+//
+//  Created by 권형일 on 12/10/24.
+//
+
+import Foundation
+
+@Observable
+class CourseCompletionViewModel {
+    let type: CourseType
+    
+    let currentPage: Int
+    var courseName = ""
+    var courseCover = ""
+    
+    init(type: CourseType, currentPage: Int) {
+        self.type = type
+        self.currentPage = currentPage
+        initCourseName()
+    }
+    
+    private func initCourseName() {
+        switch type {
+            
+        case .basicEconomy:
+            self.courseName = "I 기초 경제"
+            self.courseCover = "BasicEconomyCourseCover"
+            
+        case .priceLevel:
+            self.courseName = "II 물가"
+            self.courseCover = "PriceLevelCourseCover"
+            
+        case .unEmployment:
+            self.courseName = "III 실업"
+            self.courseCover = "UnEmploymentCourseCover"
+            
+        case .moneyAndFinance:
+            self.courseName = "IV 화폐와 금융"
+            self.courseCover = "MoneyAndFinanceCourseCover"
+            
+        case .exchangeRateAndBalanceOfPayment:
+            self.courseName = "V 환율과 국제수지"
+            self.courseCover = "ExchangeRateAndBalanceOfPaymentCourseCover"
+        }
+    }
+    
+    func updateUserCoursePage() async {
+        CourseManager.updateUserCoursePageRoute(type: type, isEnd: true, currentPage: currentPage)
+        
+        // DB User의 lastPage 값 바꾸기 (1페이지로 초기화)
+        await CourseManager.updateUserCourseLastPage(courseType: type, lastPage: 1)
+    }
+}
